302a1108183653a717c0467e486be41026bab993
Manish Ghimire <92134715+manishghimiree@users.noreply.github.com>
2023-07-12 20:00:33 +0545

    pages design

---
+++ src/components/About.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div name="about" className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'>
+        <div className='max-w-screen-lg mx-auto flex flex-col justify-center w-full h-full'>
+            <div className='pb-8'>
+                <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
+            </div>
+            <p>
+Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae, reiciendis eius. Autem, at voluptatibus modi voluptates harum minus enim error in consectetur nesciunt assumenda, molestias culpa eveniet? Corrupti, atque quidem!
+            </p>
+
+            <br/>
+
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi doloribus, asperiores enim recusandae culpa est ea aliquid, omnis quas porro fugit placeat eligendi amet magnam qui adipisci nemo quidem, dolor quia at necessitatibus aperiam saepe? Rerum quam at placeat harum modi! Magni perferendis deserunt laboriosam animi atque adipisci dolor quae.
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default About
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
   return (
     <div
       name="home"
-      className=" h-screen bg-gradient-to-b from-black to-gray-800 "
+      className=" w-full h-screen bg-gradient-to-b from-black via-black to-gray-800 "
     >
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
         <div className="flex flex-col justify-center h-full">
@@ -19,10 +19,10 @@ const Home = () => {
             React, Tailwind, Next JS and GraphQL.
           </p>
           <div>
-            <button>
+            <button className=" group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer">
               Portfolio
-              <span>
-                <MdKeyboardArrowRight />
+              <span className="group-hover:rotate-90 duration-300">
+                <MdKeyboardArrowRight size={25}  className="ml-1"/>
               </span>
             </button>
           </div>
