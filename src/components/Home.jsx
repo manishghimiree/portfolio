e9f0ec2ca59861bab0a5ada45f49f96568388dc9
Manish Ghimire <92134715+manishghimiree@users.noreply.github.com>
2023-07-12 21:27:40 +0545

    project done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import HeroImage1 from "../assets/heroImage1.jpg";
 import { MdKeyboardArrowRight } from "react-icons/md";
+import { Link } from "react-scroll";
 
 const Home = () => {
   return (
@@ -19,12 +20,13 @@ const Home = () => {
             React, Tailwind, Next JS and GraphQL.
           </p>
           <div>
-            <button className=" group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer">
+            <Link to="portfolio"
+            smooth duration={500} className=" group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer">
               Portfolio
               <span className="group-hover:rotate-90 duration-300">
                 <MdKeyboardArrowRight size={25} className="ml-1" />
               </span>
-            </button>
+            </Link>
           </div>
         </div>
         <div >
